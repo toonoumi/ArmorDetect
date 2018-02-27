21df9a25d6875acfc18ff0fe5923fe2f14bae950
toonoumi <toonoumi@vt.edu>
2018-02-27 10:23:35 -0500

    merge to master

---
+++ src/AD_Util.cpp
@@ -0,0 +1,306 @@
+//
+//  AD_Util.h
+//  ArmorDetect
+//														Dates
+//  Created by: Jason Lu			10/12/17.
+//			Edited: Frank Eyenga 	12/17/2017 
+//
+
+
+#ifdef DEBUG
+#include <iostream>
+#endif
+ 
+#include "AD_Util.h"
+
+using std::pair;
+using std::vector;
+using std::array;
+
+typedef std::vector<Mat> Matvector;
+
+AD_Util::AD_Util(){}
+
+AD_Util::~AD_Util(){}
+
+bool AD_Util::display_Img(Mat frame){}
+ 
+bool AD_Util::draw_Aim(AimPoint aim, Mat&frame){}
+
+bool AD_Util::draw_Arrow(DirectionHint hint, Mat&frame){}
+ 
+float AD_Util::find_Exposure_Adjustment(Matvector frames){}
+
+float AD_Util::find_Temperature_Adjustment(Matvector frames){}
+
+std::pair<HSVRange, bool> AD_Util::find_Red_HSVRange(Matvector frames)
+{
+	// Previous HSVRange values that worked
+	static vector<HSVRange> previous;
+	// Convert to HSV
+	Matvector frames_hsv (bgrToHSV(frames, true));
+									 
+	// Try previously successful ranges first
+	for (const auto& range : previous)
+	{
+		vector<Matvector> results = testFrames(range, frames);
+	
+		if (results.front().size() >= (frames.size() * FRAMES_THRES))
+			return pair<HSVRange, bool>(range, true);
+	}
+
+
+	// Initialize default ranges to start with
+	HSVRange cand_low {.LowH = RED_HUE_LOW, .HighH = RED_HUE_HIGH, 
+				   		 .LowS = RED_SAT_LOW, .HighS = RED_SAT_HIGH, 
+				   		 .LowV = RED_VAL_LOW, .HighV = RED_VAL_HIGH };
+
+	HSVRange cand_upp {.LowH = 172, .HighH = 180, 
+				   		 .LowS = RED_SAT_LOW, .HighS = RED_SAT_HIGH, 
+				   		 .LowV = RED_VAL_LOW, .HighV = RED_VAL_HIGH };
+								 
+	Matvector test_frames (frames_hsv), passed, failed;
+	bool calibrated = false;
+	
+	#ifdef DEBUG
+		std::cout<< "starting range finder\n\n";
+		std::cout<< "# of Frames: " << frames.size() << "\n";
+		std::cout<< "# to pass: " << frames.size() * FRAMES_THRES << "\n";
+		int pass = 0;
+	#endif
+
+	// Try to lower end of the spectrum
+	while (!calibrated && ( cand_low.LowS > RED_SAT_MIN || cand_low.LowV > RED_VAL_MIN ))
+	{ 
+		vector<Matvector> results_low (testFrames(cand_low, test_frames));
+		passed.insert(passed.end(), results_low.front().begin(), results_low.front().end());
+		
+		#ifdef DEBUG
+			std::cout<< "--Pass #" << pass++ << "\n";					
+			std::cout<< "--Passing frames: " << passed.size() << "\n";
+		#endif
+		
+		if (passed.size() >= (frames.size() * FRAMES_THRES))
+		{ 
+			calibrated = true; 
+			previous.push_back(cand_low);
+		}
+		else
+		{
+			// Reduce value by predetermined percentage
+			cand_low.LowS -= TUNER_MEDIUM(255);
+			cand_low.LowV -= TUNER_COARSE(255);
+			test_frames = results_low.back();
+		} 
+		
+		#ifdef DEBUG
+			std::cout<< "--Low sat: "  << cand_low.LowS << "\n";
+			std::cout<< "--Low val: " << cand_low.LowV << "\n\n";
+		#endif
+																			
+	}									
+	
+	if(calibrated)
+		return pair<HSVRange, bool>(cand_low, calibrated);
+		
+	// Try upper end of sprectrum
+	while (!calibrated && ( cand_upp.LowS > RED_SAT_MIN || cand_upp.LowV > RED_VAL_MIN ))
+	{ 
+		vector<Matvector> results_upp (testFrames(cand_upp, test_frames));
+		passed.insert(passed.end(), results_upp.front().begin(), results_upp.front().end());
+		
+		#ifdef DEBUG
+			std::cout<< "--Pass #" << pass++ << "\n";					
+			std::cout<< "--Passing frames: " << passed.size() << "\n";
+		#endif
+		
+		if (passed.size() >= (frames.size() * FRAMES_THRES))
+		{ 
+			calibrated = true; 
+			previous.push_back(cand_upp);
+		}
+		else
+		{
+			// Reduce value by predetermined percentage
+			cand_upp.LowS -= TUNER_MEDIUM(255);
+			cand_upp.LowV -= TUNER_COARSE(255);
+			test_frames = results_upp.back();
+		} 
+		
+		#ifdef DEBUG
+			std::cout<< "--Low sat: "  << cand_upp.LowS << "\n";
+			std::cout<< "--Low val: " << cand_upp.LowV << "\n\n";
+		#endif
+																			
+	}
+								  
+	return pair<HSVRange, bool>(cand_upp, calibrated);
+}
+
+std::pair<HSVRange, bool> AD_Util::find_Blue_HSVRange(Matvector frames)
+{
+	// Previous HSVRange values that worked
+	static vector<HSVRange> previous;
+	// Convert to HSV
+	Matvector frames_hsv (bgrToHSV(frames, true));
+									 
+	// Try previously successful ranges first
+	for (const auto& range : previous)
+	{
+		vector<Matvector> results = testFrames(range, frames);
+	
+		if (results.front().size() >= (frames.size() * FRAMES_THRES))
+			return pair<HSVRange, bool>(range, true);
+	}
+
+
+	// Initialize default ranges to start with
+	HSVRange cand {.LowH = BLUE_HUE_LOW, .HighH = BLUE_HUE_HIGH, 
+								 .LowS = BLUE_SAT_LOW, .HighS = BLUE_SAT_HIGH, 
+								 .LowV = BLUE_VAL_LOW, .HighV = BLUE_VAL_HIGH };
+								 
+	Matvector test_frames (frames_hsv), passed, failed;
+	bool calibrated = false;
+	
+	#ifdef DEBUG
+		std::cout<< "starting range finder\n\n";
+		std::cout<< "# of Frames: " << frames.size() << "\n";
+		std::cout<< "# to pass: " << frames.size() * FRAMES_THRES << "\n";
+		int pass = 0;
+	#endif
+
+	while (!calibrated && ( cand.LowS > BLUE_SAT_MIN || cand.LowV > BLUE_VAL_MIN ))
+	{ 
+		vector<Matvector> results (testFrames(cand, test_frames));
+		passed.insert(passed.end(), results.front().begin(), results.front().end());
+		
+		#ifdef DEBUG
+			std::cout<< "--Pass #" << pass++ << "\n";					
+			std::cout<< "--Passing frames: " << passed.size() << "\n";
+		#endif
+		
+		if (passed.size() >= (frames.size() * FRAMES_THRES))
+		{ 
+			calibrated = true; 
+			previous.push_back(cand);
+		}
+		else
+		{
+			// Reduce value by predetermined percentage
+			cand.LowS -= TUNER_MEDIUM(255);
+			cand.LowV -= TUNER_COARSE(255);
+			test_frames = results.back();
+		} 
+		
+		#ifdef DEBUG
+			std::cout<< "--Low sat: "  << cand.LowS << "\n";
+			std::cout<< "--Low val: " << cand.LowV << "\n\n";
+		#endif
+																			
+	}									
+	 
+									  
+	return pair<HSVRange, bool>(cand, calibrated);
+}
+
+Matvector AD_Util::threshMask(Matvector& frames, HSVRange& range)
+{
+	Matvector frames_hsv (bgrToHSV(frames, true)), masks;
+		
+	for (const auto& frame : frames_hsv)
+	{
+		Mat mask;
+		Scalar lower (range.LowH, range.LowS, range.LowV);
+		Scalar upper (range.HighH, range.HighS, range.HighV);			
+		inRange(frame, lower, upper, mask);
+		masks.push_back(mask);	
+	}
+	
+	return masks;
+}
+
+Matvector AD_Util::bgrToHSV(Matvector& frames, bool blur)
+{
+	Matvector frames_hsv;
+	
+	for (auto& frame : frames)
+	{
+		Mat tmp;
+
+		if (blur)
+			GaussianBlur(frame, tmp, Size(9, 9), 3, 3);
+		
+		cvtColor(tmp, tmp, COLOR_BGR2HSV);
+		frames_hsv.push_back(tmp);
+	}
+	
+	return frames_hsv;
+}
+
+vector<Matvector> AD_Util::testFrames(const HSVRange& range, const Matvector& frames)
+{
+	Matvector passed, failed;
+		
+	for (const auto& frame : frames)
+		{
+			// find pixels that match range
+			Mat mask;
+			Scalar lower (range.LowH, range.LowS, range.LowV);
+			Scalar upper (range.HighH, range.HighS, range.HighV);			
+			inRange(frame, lower, upper, mask);
+			
+			// use a predetermined area of the image for calibration
+			Range rows = ROI_RANGE(frame.rows);
+			Range cols = ROI_RANGE(frame.cols);
+			Mat roi = mask(rows, cols);
+			
+			uint32_t  pixelsfound = 0;
+
+			// count the number of pixels found in ROI
+			for (unsigned int i = 0; i < roi.rows; i++)
+			{
+				uint8_t* row = roi.ptr<uint8_t>(i);
+	
+				for (unsigned int j = 0; j < roi.cols; j++)
+					if (row[j]) pixelsfound++; 
+			}
+			
+			#ifdef DEBUG
+				std::cout<< "\tPixels needed: " << roi.total() * PIXELS_THRES << "\n";
+				std::cout<< "\tPixels found: " << pixelsfound << "\n";			
+			#endif
+			
+			if (pixelsfound >= (roi.total() * PIXELS_THRES))
+				{ 
+					passed.push_back(frame); 
+					#ifdef	DEBUG
+						std::cout<< "\tPassed\n";					
+					#endif
+				}
+			else
+				{ 
+					failed.push_back(frame);	
+					#ifdef DEBUG
+						std::cout<< "\tfailed\n"; 										
+					#endif
+				}
+				
+			#ifdef DEBUG
+				namedWindow("Mask", WINDOW_NORMAL);
+				imshow("Mask", mask);
+				namedWindow("Image (HSV)", WINDOW_NORMAL);
+				imshow("Image (HSV)", frame);
+
+				waitKey(0);		
+			#endif							
+		}
+		
+		#ifdef DEBUG
+			destroyWindow("Mask");
+			destroyWindow("Image (HSV)");
+		#endif
+		
+		return vector<Matvector>({passed, failed});
+
+}
+/* AD_Util_cpp */
--- src/AD_Util.h
+++ src/AD_Util.h
@@ -1,148 +1,221 @@
 //
 //  AD_Util.h
 //  ArmorDetect
+//														Dates
+//  Created by: Jason Lu			10/12/17.
+//			Edited: Frank Eyenga 	12/17/2017
 //
-//  Created by Jason Lu on 10/12/17.
-//
+
 
 #ifndef AD_Util_h
 #define AD_Util_h
+#include <vector>
+#include <utility>
 #include "opencv2/opencv.hpp"
 #include "opencv2/core.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/features2d/features2d.hpp"
 #include "opencv2/imgproc.hpp"
-#include <stack>
+
+
+// Default values for Blue and Red HSV range
+#define BLUE_HUE_LOW   	75
+#define BLUE_HUE_HIGH	130
+#define BLUE_SAT_LOW   	200
+#define BLUE_SAT_HIGH	255
+#define BLUE_VAL_LOW	200
+#define BLUE_VAL_HIGH 	255
+#define BLUE_SAT_MIN   	85
+#define BLUE_VAL_MIN   	50
+
+#define RED_HUE_LOW   	0
+#define RED_HUE_HIGH	8
+#define RED_SAT_LOW   	200
+#define RED_SAT_HIGH    255
+#define RED_VAL_LOW   	200
+#define RED_VAL_HIGH  	255
+#define RED_SAT_MIN   	85
+#define RED_VAL_MIN   	50
+
+#define U_FLOOR(x)   ( (x) < 0 ? 0 : (x) )
+#define U8_CEILING(x)  ( (x) > 255 ? 255 : (x) )
+#define ROI_CONSTANT   3
+#define ROI(x)     ((x) / ROI_CONSTANT)
+#define ROI_RANGE(x)   Range(ROI(x), (x) - ROI(x))
+#define PIXELS_THRES   0.65
+#define FRAMES_THRES   0.70
+#define TUNER_MEDIUM(x)     ((x) * 0.10)
+#define TUNER_COARSE(x)     ((x) * 0.15)
+
+
 using namespace cv;
-struct _AimPoint{
-    float x;
-    float y;
-    float offset_x;
-    float offset_y;
+
+struct _AimPoint {
+  float x;
+  float y;
+  float offset_x;
+  float offset_y;
 };
 typedef struct _AimPoint AimPoint;
 
-struct _WindowRect{
-    int x;
-    int y;
-    int width;
-    int height;
+struct _WindowRect {
+  int x;
+  int y;
+  int width;
+  int height;
 };
+
 typedef struct _WindowRect WindowRect;
 
-struct _HSVRange{
-    int LowH;
-    int HighH;
-    int LowS;
-    int HighS;
-    int LowV;
-    int HighV;
+struct _HSVRange {
+  int LowH;
+  int HighH;
+  int LowS;
+  int HighS;
+  int LowV;
+  int HighV;
 };
 typedef struct _HSVRange HSVRange;
 
-enum DirectionHint{MOVE_UP=1,MOVE_DOWN,MOVE_LEFT,MOVE_RIGHT};
+enum DirectionHint {
+  MOVE_UP = 1, MOVE_DOWN, MOVE_LEFT, MOVE_RIGHT
+};
 
-class AD_Util{
+class AD_Util {
 public:
-    /**
-     * The constructor of AD_Util class
-     *
-     */
-    AD_Util();
-    /**
-     * Deconstructor of AD_Util class
-     */
-    ~AD_Util();
-    /**
-     * This is to display a video or a image. Instead of using imshow
-     * we will be using this function. It will organize the windows so that
-     * they do not overlap each other when displaying on screen
-     * @param frame
-     *      The Mat image to display
-     * @return
-     *      if the image is successfully displayed
-     */
-    bool display_Img(Mat frame); 
-    /**
-     * This will draw a aim object on a frame, like a first personal shooting game HUD
-     * offset is for zeroing purpose.
-     * make it cool
-     *
-     * @param frame
-     *      The Mat image to draw the HUD (in BGR)
-     * @param aim
-     *      The location to draw the aimming HUD
-     * @return
-     *      if the HUD is drew
-     */
-    bool draw_Aim(AimPoint aim, Mat&frame); //Hanson
-    /**
-     * This will draw arrows to the directions we want to go. (Draw it by the borders of the
-     * frame, make it a decent UI)
-     * ex: If the Aim is on the left of our target, client code will invoke this function
-     * to draw a right arrow.
-     * @param frame
-     *      The Mat image to draw the hint (in BGR)
-     * @param hint
-     *      The direction himt we want to draw
-     * @return
-     *      if the HUD is drew
-     */
-    bool draw_Arrow(DirectionHint hint, Mat&frame);
-    /**
-     * This will find exposure adjustment that our program needs to make to the camera
-     * for it to clearly see objects. Hint: calculate several frames and average the results.
-     *
-     *
-     * scale: -12 to 12
-     * @param frames
-     *      The Mat images for it to make decision
-     * @return
-     *      the adjustment it needs to make to current exposure
-     */
-    float find_Exposure_Adjustment(std::stack<Mat> frames); //andy
-    /**
-     * This will find temperature adjustment that our program needs to make to the camera
-     * for it to clearly see objects and identify colors.
-     * Hint: calculate several frames and average the results.
-     *
-     *
-     * scale: -12 to 12
-     * @param frames
-     *      The Mat images for it to make decision (in BGR)
-     * @return
-     *      the adjustment it needs to make to current exposure
-     */
-    float find_Temperature_Adjustment(std::stack<Mat> frames); //hanyi
-    /**
-     * This will find correct HSV range for identifying the concept "RED object"
-     * for calibration purpose. Place a plain red object in front of the camera,
-     * until it sees majority of the points in a frame to pass the threshhold, report the range
-     * *May requires understanding of PID algorithm
-     * @param frames
-     *      The Mat images for it to make decision (in HSV)
-     * @return
-     *      The correct HSV range to identify red objects
-     */
-    HSVRange find_Red_HSVRange(std::stack<Mat> frames); //frank
-    /**
-     * This will find correct HSV range for identifying the concept "BLUE objects"
-     * for calibration purpose. Place a plain red object in front of the camera,
-     * until it sees majority of the points in a frame to pass the threshhold, report the range
-     * *May requires understanding of PID algorithm
-     * @param frames
-     *      The Mat images for it to make decision (in HSV)
-     * @return
-     *      The correct HSV range to identify blue objects
-     */
-    HSVRange find_Blue_HSVRange(std::stack<Mat> frames);
-    /**
-     *
-     */
+  /**
+   * The constructor of AD_Util class
+   *
+   */
+  AD_Util();
+
+  /**
+   * Deconstructor of AD_Util class
+   */
+  ~AD_Util();
+
+  /**
+   * This is to display a video or a image. Instead of using imshow
+   * we will be using this function. It will organize the windows so that
+   * they do not overlap each other when displaying on screen
+   * @param frame
+   *      The Mat image to display
+   * @return
+   *      if the image is successfully displayed
+   */
+  bool display_Img(Mat frame);
+
+  /**
+   * This will draw a aim object on a frame, like a first personal shooting game HUD
+   * offset is for zeroing purpose.
+   * make it cool
+   *
+   * @param frame
+   *      The Mat image to draw the HUD (in BGR)
+   * @param aim
+   *      The location to draw the aimming HUD
+   * @return
+   *      if the HUD is drew
+   */
+  bool draw_Aim(AimPoint aim, Mat&frame);
+  /**
+   * This will draw arrows to the directions we want to go. (Draw it by the borders of the
+   * frame, make it a decent UI)
+   * ex: If the Aim is on the left of our target, client code will invoke this function
+   * to draw a right arrow.
+   * @param frame
+   *      The Mat image to draw the hint (in BGR)
+   * @param hint
+   *      The direction himt we want to draw
+   * @return
+   *      if the HUD is drew
+   */
+  bool draw_Arrow(DirectionHint hint, Mat&frame);
+
+  /**
+   * This will find exposure adjustment that our program needs to make to the camera
+   * for it to clearly see objects. Hint: calculate several frames and average the results.
+   *
+   *
+   * scale: -12 to 12
+   * @param frames
+   *      The Mat images for it to make decision
+   * @return
+   *      the adjustment it needs to make to current exposure
+   */
+  float find_Exposure_Adjustment(std::vector<Mat> frames);
+
+  /**
+   * This will find temperature adjustment that our program needs to make to the camera
+   * for it to clearly see objects and identify colors.
+   * Hint: calculate several frames and average the results.
+   *
+   *
+   * scale: -12 to 12
+   * @param frames
+   *      The Mat images for it to make decision (in BGR)
+   * @return
+   *      the adjustment it needs to make to current exposure
+   */
+  float find_Temperature_Adjustment(std::vector<Mat> frames);
+
+  /**
+   * This will find correct HSV range for identifying the concept "RED object"
+   * for calibration purpose. Place a plain red object in front of the camera,
+   * until it sees majority of the points in a frame to pass the threshhold, report the range
+   * *May requires understanding of PID algorithm
+   * @param frames
+   *      The Mat images for it to make decision (in HSV)
+   * @return
+   *      The correct HSV range to identify red objects
+   */
+	std::pair<HSVRange, bool> find_Red_HSVRange(std::vector<Mat> frames);
+
+  /**
+   * This will find correct HSV range for identifying the concept "BLUE objects"
+   * for calibration purpose. Place a plain blue object in front of the camera,
+   * until it sees majority of the points in a frame to pass the threshhold, report the range
+   * *May requires understanding of PID algorithm
+   * @param frames
+   *      The Mat images for it to make decision (in HSV)
+   * @return
+   *      The correct HSV range to identify blue objects
+   */
+  std::pair<HSVRange, bool> find_Blue_HSVRange(std::vector<Mat> frames);
+
+  /**
+   * Converts images from RGB/BGR to HSV color format, applies a custom threshold, and
+   * returns the resulting binary images. In other words, each image will be
+   * converted and it's pixels will be checked against the given range. Every
+   * pixel who's HSV values are within that range will be set(1), all others will be
+   * cleared (0). These binary images are referred to as "mask."
+   * @param frames
+   *       A vector of Mat images formated in the RGB/BGR color space
+   * @param
+   *      The range of HSV value that will be used to threshold the images
+   * @return
+   *      A vector of binary Mats that resulted from the threshold operation
+   */
+  std::vector<Mat> threshMask(std::vector<Mat>& frames, HSVRange& range);
+
+
 private:
-    std::stack<WindowRect> windowsDisplayed;
-    
-    
+  std::vector<WindowRect> windowsDisplayed;
+
+  /**
+   * Converts images formated using the BGR(Blue-Green-Red) color space to their
+   * HSV equivalent. Note that the typical color format for images is RGB, but
+   * openCV automatically converts a RGB image to BGR when it is loaded.
+   * @param frames
+   *      A vector of Mat images formated in the RGB/BGR color space
+   * @param blur
+   *      A boolean value. If true then a Gaussian blur will be applied to
+   *      each image before converting it, otherwise only the conversion is done.
+   * @return
+   */
+  std::vector<Mat> bgrToHSV(std::vector<Mat>& frames, bool blur);
+  std::vector<std::vector<Mat>> testFrames(const HSVRange& range, const std::vector<Mat>& frames);
+
 };
 
 #endif /* AD_Util_h */
